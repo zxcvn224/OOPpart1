86cf5a9d0857fa10182e937ecad9e458d24ba479
Opilane TTHK <opilane@tthk.ee>
2021-11-25 09:56:25 +0200

    OOPpart1

---
+++ OOPcat/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace OOPcat
+{
+    class Program
+    {
+        class Cat
+        {
+            string name;
+            string color;
+            double deprivation_of_food;
+
+
+            public Cat(string _name, string _color)
+            {
+                name = _name;
+                color = _color;
+                deprivation_of_food = 0;
+                Console.WriteLine($"The {color} colored cat {name} has been created");
+            }
+            public string Name
+            {
+                get { return name; }
+            }
+
+            public string Color
+            {
+                get { return color; }
+            }
+
+            public double Deprivation_of_food
+            {
+                get { return deprivation_of_food; }
+            }
+
+            public void Sleep()
+            {
+                Console.WriteLine("<<<<<<>>>>>>");
+                deprivation_of_food += 0.2;
+            }
+
+            public void mjäu()
+            {
+                Console.WriteLine("Mewo");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Cat myCat = new Cat("mjä'w", "White");
+
+            while (myCat.Deprivation_of_food != 1)
+            {
+                myCat.Sleep();
+            }
+            myCat.mjäu();
+
+            Console.WriteLine($"Level of hunger: {myCat.Deprivation_of_food}");
+        }
+    }
+}
---
+++ OOPpart1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace OOPpart1
+{
+    class Program
+    {
+        class Dog
+        {
+            string name;
+            int spots;
+            double happiness;
+
+            public Dog (string _name, int _spots)
+            {
+                name = _name;
+                spots = _spots;
+                happiness = 0;
+                Console.WriteLine($"The dog {name} with {spots} spots has been created");
+            }
+
+            public string Name
+            {
+                get { return name; }
+            }
+
+            public int Spots
+            {
+                get { return spots; }
+            }
+
+            public double Happiness
+            {
+                get { return happiness; }
+            }
+
+            public void Madis()
+            {
+                Console.WriteLine("Woof-woof!");
+                happiness += 0.2; //happiness = happiness + 0.2
+            }
+
+            public void WagTail()
+            {
+                Console.WriteLine("Wiggle-Wiggle");
+            }
+
+            public void Rename(string newName)
+            {
+                name = newName;
+            }
+
+
+
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+            Dog myDog = new Dog("Madis", 15);
+            // myDog.happiness = 10;
+            // myDog.name = "Good Boy";
+
+            while(myDog.Happiness !=1)
+            {
+                myDog.Madis();
+            }
+
+            myDog.WagTail();
+
+            Console.WriteLine("Rename your dog:");
+            string userInput = Console.ReadLine();
+            myDog.Rename(userInput);
+
+            Console.WriteLine($"{myDog.Name}");
+            Console.WriteLine($"Level of happiness: {myDog.Happiness}");
+        }
+    }
+}
